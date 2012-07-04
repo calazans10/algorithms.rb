aed6481b03a94cc18d29b55dae904f1888572057
Jeferson Farias Calazans <calazans10@gmail.com>
2012-07-04 16:01:33 -0300

    corrigindo erro no arquivo paridade

--- 08/paridade.rb
+++ 08/paridade.rb
@@ -1,4 +1,4 @@
 def paridade(numero)
-  paridade = numero.to_s(2).split('0').map { |x| x.to_i }.reduce(:+) unless numero <= 0
+  numero.to_s(2).split('0').map { |x| x.to_i }.reduce(:+) unless numero <= 0
 end
 
