8c7427842da9eb1e1b27d7e46975f771686e1c11
Jeferson Farias Calazans <calazans10@gmail.com>
2012-07-04 16:13:40 -0300

    removendo erros da função e adicionando testes

--- 08/paridade.rb
+++ 08/paridade.rb
@@ -1,4 +1,4 @@
 def paridade(numero)
-  numero.to_s(2).split('0').map { |x| x.to_i }.reduce(:+) unless numero <= 0
+  numero.to_s(2).split('').map { |x| x.to_i }.reduce(:+) unless numero <= 0
 end
 
--- 08/paridade_spec.rb
+++ 08/paridade_spec.rb
@@ -6,15 +6,23 @@ describe "paridade" do
   it "should return 1 to paridade of 1" do
     paridade(1).should eq(1)
   end
+
   it "should return 1 to paridade of 2" do
     paridade(2).should eq(1)
   end
+
   it "should return 2 to paridade of 10" do
     paridade(10).should eq(2)
   end
+
   it "should return 3 to paridade of 21" do
     paridade(21).should eq(3)
   end
+
+  it "should return 31 to paridade of 2147483647" do
+    paridade(2147483647).should eq(31)
+  end
+
   it "should return nil to paridade under 0" do
     paridade(0).should eq(nil)
     paridade(-10).should eq(nil)
