dd612b0c7086b6dfc8d90fb182d58024e5abb51c
Jeferson Farias Calazans <calazans10@gmail.com>
2012-07-04 15:58:17 -0300

    resolvendo exercicio que calcula a paridade de um numero

---
+++ 08/paridade_spec.rb
@@ -0,0 +1,22 @@
+#encoding: utf-8
+
+require "./paridade"
+
+describe "paridade" do
+  it "should return 1 to paridade of 1" do
+    paridade(1).should eq(1)
+  end
+  it "should return 1 to paridade of 2" do
+    paridade(2).should eq(1)
+  end
+  it "should return 2 to paridade of 10" do
+    paridade(10).should eq(2)
+  end
+  it "should return 3 to paridade of 21" do
+    paridade(21).should eq(3)
+  end
+  it "should return nil to paridade under 0" do
+    paridade(0).should eq(nil)
+    paridade(-10).should eq(nil)
+  end
+end
